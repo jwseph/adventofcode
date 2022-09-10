32ce7ec65dfd5a3f395ba852b67fd08209fda277
kamiakhs <admin@kamiak.org>
2022-09-10 16:49:24 -0700

    a

--- 13/Day13A.java
+++ 13/Day13A.java
@@ -13,23 +13,20 @@ public class Day13A {
             String _s[] = s.split("\\,");
             arr.add(Integer.parseInt(_s[0])*BASE+Integer.parseInt(_s[1]));
         }
-        while (in.hasNext()) {
-            String _s[] = in.nextLine().split("=");
-            char f = _s[0].charAt(_s[0].length()-1);
-            int v = Integer.parseInt(_s[1])+1;
-            Set<Integer> arr2 = new HashSet<Integer>();
-            if (f == 'x') {
-                for (int num: arr) {
-                    int x = num/BASE, y = num%BASE;
-                    if (x < v) {
-                        // arr2.add()
-                    }
-                    
-                }
-            } else {
-
-            }
+        String _s[] = in.nextLine().split("=");
+        char f = _s[0].charAt(_s[0].length()-1);
+        int v = Integer.parseInt(_s[1]);
+        Set<Integer> res = new HashSet<Integer>();
+        if (f == 'x') for (int num: arr) {
+            int x = num/BASE, y = num%BASE;
+            x = (x < v) ? x : (2*v-x);
+            res.add(x*BASE+y);
+        } else for (int num: arr) {
+            int x = num/BASE, y = num%BASE;
+            y = (y < v) ? y : (2*v-y);
+            res.add(x*BASE+y);
         }
 
+        System.out.println(res.size());
     }
 }
