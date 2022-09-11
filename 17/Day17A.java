0a1c14e7ff54776740de0f71766ea7826b977448
kamiakhs <admin@kamiak.org>
2022-09-11 15:07:00 -0700

    update

---
+++ 17/Day17A.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Day17A {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("Day17.in"));
+        // First find Y velocities that will work using n*(n+1)/2
+        // Then check if x matches?
+    }
+}
