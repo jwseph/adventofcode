0786589d65f697b3d2a4f2d15de4cd44864d33bb
kamiakhs <admin@kamiak.org>
2022-09-13 06:30:25 -0700

    a

--- 18/Day18A.java
+++ 18/Day18A.java
@@ -3,23 +3,25 @@ import java.io.*;
 
 public class Day18A {
     private static Scanner in;
-    private static class Number {
+    private static class SnailfishNumber {
         Pair parent;
+        SnailfishNumber(Pair parent) { this.parent = parent; }
+        Number getLeft() { return parent == null ? null : equals(parent.l) ? parent.getLeft() : parent.l instanceof Number ? (Number)parent.l: ((Pair)parent.l).getLast(); }
+        Number getRight() { return parent == null ? null : equals(parent.r) ? parent.getRight() : parent.r instanceof Number ? (Number)parent.r: ((Pair)parent.r).getFirst(); }
+    }
+    private static class Number extends SnailfishNumber {
         Integer val;
         Number(Pair parent, String c) {
-            this.parent = parent;
+            super(parent);
             val = Integer.valueOf(c);
         }
         public String toString() { return val.toString(); }
     }
-    private static class Pair {
-        Pair parent;
+    private static class Pair extends SnailfishNumber {
         Object l, r;
-        Pair() {
-            this(null);
-        }
+        Pair() { this(null); }
         Pair(Pair parent) {
-            this.parent = parent;
+            super(parent);
             String c = in.next();
             l = (c.equals("[")) ? new Pair(this) : new Number(this, c);
             in.next();
@@ -28,21 +30,18 @@ public class Day18A {
             in.next();
             return;
         }
-        Pair(Pair l, Pair r) {
-            this(null, l, r);
-        }
+        Pair(Pair l, Pair r) { this(null, l, r); }
         Pair(Pair parent, Pair l, Pair r) {
-            this.parent = parent;
+            super(parent);
             this.l = l;
             this.r = r;
             l.parent = this;
             r.parent = this;
         }
-        Number getLeft() { return parent == null ? null : equals(parent.l) ? parent.getLeft() : parent.l instanceof Number ? (Number)parent.l: ((Pair)parent.l).getLast(); }
-        Number getRight() { return parent == null ? null : equals(parent.r) ? parent.getRight() : parent.r instanceof Number ? (Number)parent.r: ((Pair)parent.r).getFirst(); }
+        public String toString() { return "["+l.toString()+","+r.toString()+"]"; }
+        static Pair sum(Pair l, Pair r) { return new Pair(l, r); }
         Number getLast() { return r instanceof Number ? (Number)r : ((Pair)r).getLast(); }
         Number getFirst() { return l instanceof Number ? (Number)l : ((Pair)l).getFirst(); }
-        public String toString() { return "["+l.toString()+","+r.toString()+"]"; }
     }
     public static void main(String[] args) throws FileNotFoundException {
         in = new Scanner(new File("Day18.in")).useDelimiter("\\r*\\n*");
