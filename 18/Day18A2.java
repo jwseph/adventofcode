1697a671c274f8f8c756b0581d8d15cbbdbb46a0
kamiakhs <admin@kamiak.org>
2022-09-13 18:24:37 -0700

    a

--- 18/Day18A2.java
+++ 18/Day18A2.java
@@ -2,51 +2,79 @@ import java.util.*;
 import java.io.*;
 
 public class Day18A2 {
-    // private static List<Character> s = new ArrayList<Character>();
-    // private static int split(int i, int d) {
-    //     int k = 0;
-    //     for (int j = i; j >= 0; j--) {
-    //         if (s.get(j) == '[') {
-    //             d--;
-    //         } else if (s.get(j) == ']') {
-    //             d++;
-    //         } else if ('0' <= s.get(j) && s.get(j) <= '9') {
-    //             int v = s.get(i)-'0'+s.get(j)-'0';
-    //             if (v > 9) k += explode(j, d, v);
-    //         }
-    //     }
-    // }
-    // private static int explode(int i, int d, int v) {
-    //     int k = 0;
-    //     s.remove(i);
-    //     s.add(i, '[');
-    //     s.add(i, (char)(v/2+'0'));
-    //     s.add(i, ',');
-    //     s.add(i, (char)(v-v/2+'0'));
-    //     s.add(i, ']');
-    //     k += 4;
-    //     d++;
-    //     if (d > 4) k += split(i, d);
-    //     return k;
-    // }
+    private static List<Character> s = new ArrayList<Character>();
+    private static boolean solve() {
+        int d = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s.get(i) == '[') {
+                d++;
+                continue;
+            }
+            if (s.get(i) == ']') {
+                if (d > 4) {
+                    System.out.println(i);
+                    i -= 4;
+                    int l = s.get(i+1)-'0', r = s.get(i+3)-'0';
+                    for (int j = 0; j < 5; j++) s.remove(i);
+                    s.add(i, '0');
+                    boolean b = false;
+                    for (int j = i+1; j < s.size(); j++) {
+                        if ('0' <= s.get(j) && s.get(j) <= '9') {
+                            int v = r+s.get(j)-'0';
+                            System.out.println(j);
+                            if (v > 9) {
+                                s.remove(j);
+                                s.add(j, '[');
+                                s.add(j, (char)(v/2+'0'));
+                                s.add(j, ',');
+                                s.add(j, (char)(v-v/2+'0'));
+                                s.add(j, ']');
+                                b = true;
+                                return false;
+                            } else {
+                                s.set(j, (char)(v+'0'));
+                            }
+                            break;
+                        }
+                    }
+                    // for (int j = i-1; j >= 0; j--) {
+                    //     if ('0' <= s.get(j) && s.get(j) <= '9') {
+                    //         int v = l+s.get(j)-'0';
+                    //         if (v > 9) {
+                    //             s.remove(j);
+                    //             s.add(j, '[');
+                    //             s.add(j, (char)(v/2+'0'));
+                    //             s.add(j, ',');
+                    //             s.add(j, (char)(v-v/2+'0'));
+                    //             s.add(j, ']');
+                    //             b = true;
+                    //             return false;
+                    //         } else {
+                    //             s.set(j, (char)(v+'0'));
+                    //         }
+                    //         break;
+                    //     }
+                    // }
+                    if (b) return false;
+                }
+                d--;
+                continue;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) throws FileNotFoundException {
-        // Scanner in = new Scanner(new File("Day18.in"));
+        Scanner in = new Scanner(new File("Day18.in"));
         
-        // String str = in.next();
-        // while (in.hasNext()) str = "["+str+","+in.next()+"]";
-        // for (char c: str.toCharArray()) s.add(c);
+        String str = in.next();
+        while (in.hasNext()) str = "["+str+","+in.next()+"]";
+        for (char c: str.toCharArray()) s.add(c);
 
-        // int d = 0;
-        // for (int i = 0; i < s.size(); i++) {
-        //     if (s.get(i) == '[') {
-        //         d++;
-        //         return;
-        //     }
-        //     if (s.get(i) == ']') {
-        //         if (d > 4) split(i-4, d);
-        //         d--;
-        //         return;
-        //     }
-        // }
+        for (Character c: s) System.out.print(c); System.out.println();
+        solve();
+        System.out.println();
+        // boolean b = solve();
+        // while (!b) solve();
+        for (Character c: s) System.out.print(c); System.out.println();
     }
 }
