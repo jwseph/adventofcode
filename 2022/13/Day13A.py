d759cd1b42bd2d8711e0c0b6ef602345946ecc6b
jwseph <kodomo.oishii@gmail.com>
2022-12-15 17:25:13 -0800

    a

--- 2022/13/Day13A.py
+++ 2022/13/Day13A.py
@@ -1,21 +1,15 @@
 s = [pair.split('\n') for pair in open("Day13.in").read().strip().split("\n\n")]
+
 def compare(l, r):
-    if isinstance(l, list) and isinstance(r, int):
-        r = [r]
-    if isinstance(l, int) and isinstance(r, list):
-        l = [l]
-    if isinstance(l, int):
-        return -1 if l < r else 1 if l > r else 0
+    if isinstance(l, list) and isinstance(r, int): r = [r]
+    if isinstance(l, int) and isinstance(r, list): l = [l]
+    if isinstance(l, int): return l-r
     for l_, r_ in zip(l, r):
         c = compare(l_, r_)
-        if c < 0: return -1
-        if c > 0: return 1
-    if len(l) < len(r): return -1
-    if len(l) > len(r): return 1
-    return 0
+        if c != 0: return c
+    return len(l)-len(r)
 
 res = 0
-for i, (l_s, r_s) in enumerate(s):
-    l, r = eval(l_s), eval(r_s)
-    if compare(l, r) <= 0: res += i+1
+for i, (l, r) in enumerate(s):
+    if compare(eval(l), eval(r)) <= 0: res += i+1
 print(res)
--- 2022/13/Day13B.py
+++ 2022/13/Day13B.py
@@ -1,23 +1,17 @@
 from functools import cmp_to_key
+
 lines = [line for line in open("Day13.in").read().strip().split("\n") if line]
+
 def compare(l, r):
-    if isinstance(l, list) and isinstance(r, int):
-        r = [r]
-    if isinstance(l, int) and isinstance(r, list):
-        l = [l]
-    if isinstance(l, int):
-        return -1 if l < r else 1 if l > r else 0
+    if isinstance(l, list) and isinstance(r, int): r = [r]
+    if isinstance(l, int) and isinstance(r, list): l = [l]
+    if isinstance(l, int): return l-r
     for l_, r_ in zip(l, r):
         c = compare(l_, r_)
-        if c < 0: return -1
-        if c > 0: return 1
-    if len(l) < len(r): return -1
-    if len(l) > len(r): return 1
-    return 0
+        if c != 0: return c
+    return len(l)-len(r)
 
-arr = [eval(line) for line in lines]
 p1, p2 = [[2]], [[6]]
-arr.append(p1)
-arr.append(p2)
+arr = [eval(line) for line in lines]+[p1, p2]
 arr.sort(key=cmp_to_key(compare))
 print((1+arr.index(p1))*(1+arr.index(p2)))
