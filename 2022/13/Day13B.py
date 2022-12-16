caf0b670e563ba6435dca3477c2c52ebbea5a645
jwseph <kodomo.oishii@gmail.com>
2022-12-15 17:40:32 -0800

    a

--- 2022/13/Day13B.py
+++ 2022/13/Day13B.py
@@ -1,6 +1,6 @@
 from functools import cmp_to_key
 
-arr = [eval(line) for line in open("Day13.in").read().strip().split("\n") if line]
+lines = [line for line in open("Day13.in").read().strip().split("\n") if line]
 
 def compare(l, r):
     if isinstance(l, list) and isinstance(r, int): r = [r]
@@ -12,5 +12,6 @@ def compare(l, r):
     return len(l)-len(r)
 
 p1, p2 = [[2]], [[6]]
+arr = list(map(eval, lines))+[p1, p2]
 arr.sort(key=cmp_to_key(compare))
 print((1+arr.index(p1))*(1+arr.index(p2)))
